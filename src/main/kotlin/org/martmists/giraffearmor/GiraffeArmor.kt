d7cc5ecfba41ae80ffff76e508d74230175e0a6f
Martmists <martmists@gmail.com>
2018-06-25 14:39:23 +0200

    Fill out mcmod.info a bit
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/main/kotlin/org/martmists/giraffearmor/GiraffeArmor.kt
+++ src/main/kotlin/org/martmists/giraffearmor/GiraffeArmor.kt
@@ -3,7 +3,7 @@ package org.martmists.giraffearmor
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 
-@Mod(modid = "GiraffeArmor", modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter")
+@Mod(modid = "giraffearmor", modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter")
 object GiraffeArmor {
 
     @Mod.EventHandler
